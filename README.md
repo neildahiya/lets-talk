bcef5fb6157a671a85200233cedb63a703b731ae
Neil Dahiya <neildahiya1@gmail.com>
2020-01-04 23:38:05 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Repo for a Mental Health Aggregator/Assistant.
---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express"),
+  app = express(),
+  bodyParser = require("body-parser"),
+  mongoose = require("mongoose"),
+  PORT = process.env.PORT || 5000,
+  //   Layouts = require("express-ejs-layouts"),
+  session = require("express-session"),
+  flash = require("connect-flash"),
+  passport = require("passport");
+
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.use(express.urlencoded({ extended: true }));
+// app.use(Layouts);
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.listen(PORT, function() {
+  console.log(`listening to port:${PORT}`);
+});
